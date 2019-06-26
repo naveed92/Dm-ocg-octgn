c52a380502a6ba084f019d8ee6ba3e76f66ec591
PRANJALSLAPTOP\Pranjal <pranjalnitrox@gmail.com>
2019-06-26 22:23:08 +0530

    [Extraction] Folder name toggler is done

--- extractionPrograms/NetBeansJavaProject/DM_Sets_Extractor/src/dm_sets_extractor/FolderNameToggler.java
+++ extractionPrograms/NetBeansJavaProject/DM_Sets_Extractor/src/dm_sets_extractor/FolderNameToggler.java
@@ -9,28 +9,51 @@ public class FolderNameToggler {
     public static void main(String args[]) throws Exception {
         try {
             //incomplete
-            String path = "C:\\Users\\user\\Documents\\OCTGN\\GameDatabase\\bb784fc6-fe21-4603-90d7-82c049908a74\\Sets\\";
-            //boolean renameToID = true;
+            File current = new File(new File(".").getCanonicalPath());
+            String path = current.getParentFile().getParentFile().getParentFile().getCanonicalPath();
+            path = path + "\\GameDatabase\\bb784fc6-fe21-4603-90d7-82c049908a74\\Sets\\";
 
             File root = new File(path);
-            String imageRootPath = path.replace("GameDatabase", "ImageDatabase");
+            String imagePath = path.replace("GameDatabase", "ImageDatabase");
 
-            System.out.println("Path is " + root.getAbsoluteFile() + "\nImage Path is " + imageRootPath);
+            System.out.println("Path is " + root.getAbsoluteFile() + "\nImage Path is " + imagePath);
 
             String[] folders = root.list();
-            Boolean isNamedAsID = (folders[0].lastIndexOf("-") == folders[0].length() - 13);   //pretty loose check, 
-            //should probabably use a prompt whether to rename to set name or set ID
-            System.out.println(isNamedAsID ? "Foldes are ID names" : "Folders are set names");
+            boolean renameToNames = false;  //controls what the program renames to - names or IDs
+
+            if (folders[0].length() == 36 && folders[0].charAt(23) == '-') {
+                System.out.println("Folder names are IDs, renaming to setNames");
+                renameToNames = true;
+            } else {
+                System.out.println("Folder names are set names, renaming to setIDs");
+
+            }
 
             for (String folder : folders) {
-                System.out.println("FolderPath = "+path+folder);
+                System.out.println("FolderPath = " + path + folder);
                 File setFile = new File(path + folder + "\\set.xml");
-                File newFolder = new File(path+getNameFromFile(setFile, isNamedAsID));
+                String newFolderName = getNameFromFile(setFile, renameToNames);
+                File newFolder = new File(path + newFolderName);
+                File oldFolder = new File(path + folder);
 
-                System.out.println(path+folder +" will be renamed to "+newFolder.getCanonicalPath()); // will throw exception if fileName is invalid
+                System.out.println(path + folder + " will be renamed to " + newFolder.getCanonicalPath()); // will throw exception if fileName is invalid
 
-                File oldFolder = new File(folder);
-                //oldFolder.renameTo(newFolder);
+                if (!newFolder.exists()) {
+                    oldFolder.renameTo(newFolder);
+                } else {
+                    System.out.println("wtf Folder already exists!");
+                }
+                
+                System.out.println("Now renaming image folder...");
+                oldFolder = new File(imagePath+folder);
+                newFolder = new File(imagePath + newFolderName);
+                if (!newFolder.exists()) {
+                    oldFolder.renameTo(newFolder);
+                } else {
+                    System.out.println("wtf image Folder already exists!");
+                }
+
+                
             }
 
         } catch (Exception e) {
@@ -46,7 +69,7 @@ public class FolderNameToggler {
             line = sc.nextLine();
             int i = line.indexOf("et.xsd");
             if (i > 0) {
-                break;
+                break; //found
             }
         }
         //System.out.println("Line is "+line);
@@ -57,13 +80,16 @@ public class FolderNameToggler {
 
         int i = line.indexOf(searchParam);  //check for error here if i < 0 pls
 
-        i = i + 6;
+        i = i + 4;  //when it's id move 4 chars front
+        if (getName) {
+            i += 2;  //when it's name move 2 more
+        }
         int j = line.indexOf("\"", i);
         int k = line.indexOf(":", i);
         if (k != -1) {
             j = k;
         }
-        
+
         sc.close();
         return line.substring(i, j);
 
