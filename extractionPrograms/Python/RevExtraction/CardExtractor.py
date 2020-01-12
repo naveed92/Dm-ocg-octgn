c03de764a3162889831505ec02b2d6649bc62180
PRANJALSLAPTOP\Pranjal <pranjalnitrox@gmail.com>
2020-01-12 09:53:18 +0530

    Added more stuff.

--- extractionPrograms/Python/RevExtraction/CardExtractor.py
+++ extractionPrograms/Python/RevExtraction/CardExtractor.py
@@ -1,40 +1,75 @@
 import urllib.request
 import urllib.parse
-import re
+
+
+temp = "Smith, Breaking Right"
 
 def extractCardData(cardName):
-	with urllib.request.urlopen('https://duelmasters.fandom.com/api.php?action=query&prop=revisions&rvprop=content&format=php&titles='+cardName) as response:
-		html1 = response.read()
+	cardNameURL = urllib.parse.quote(cardName)
+	with urllib.request.urlopen('https://duelmasters.fandom.com/api.php?action=query&prop=revisions&rvprop=content&format=php&titles='+cardNameURL) as response:
+		html1 = response.read().decode('utf-8')
 
 	html=""
 	html = str(html1)
 
-	i = html.find('{{Cardtable') +26
-	j = html.find('ocgeffect')
-	if j == -1:		##Vanilla card
-		j = html.find('flavor')
-		if j == -1:
-			j = html.find('artist')
-
-	j-=1
-
+	i = html.find('{{Cardtable') +11
+	j = html.rfind('}}')
+	if j == -1:
+		j = html.find('[[Category:')
 	html = html[i:j]
-	html = html.replace('\\n','\n')
+
+	print("Point 1, HTML is :\n"+html)
+
+	##### Getting all the data #######
+	#image can be gotten through query, check the other program with Yomi
+	name = cardName.replace("_", " ")
+
+	cost = getAttribute("cost", html)
+	if cost is None:
+		print("ERROR: No cost found!")
+
+	civ = getAttribute("civilization", html)
+	if civ is None:
+		print("WARNING: No civ found! Assigning Zero.")
+		civ = "Zero"
+
+	race = getAttribute("race", html)
+	if race is None:
+		print("No race found. Not a creature?")
+
+	power = getAttribute("power", html)
+	if power is None:
+		print("No power found. Assigning 0.")
+		power = "0"
+
 
 	return(html)
+
+def getAttribute(attr, html):
+	i = html.lower().find(attr.lower()+" = ")
+	if i < 0:
+		print("ERROR FINDING "+attr)
+		return
+	i += len(attr)+3
+	j = html.find("\n", i)
+	result = html[i:j] # got 'em
+	print(attr+" is: " + result)
+
+	return result
+
 
 def parseEffect(cardData):
 	i = cardData.find('effect = ')
 	if i == -1:
 		print("No effect! Vanilla card?")
-		return
+		return ""
 	effect = cardData[(i+9):-1]
 	print("Effect is :"+effect)
 
 	effect = urllib.parse.quote(effect)
 
 	with urllib.request.urlopen('https://duelmasters.fandom.com/api.php?action=parse&format=php&text='+effect) as response:
-		html1 = response.read()
+		html1 = response.read().decode('utf-8')
 
 	html = ""
 	html = str(html1)
@@ -69,3 +104,20 @@ def clean(text):
 				inTag = False
 		
 	return result
+
+
+
+
+result = extractCardData(temp)
+#print(result)
+effText = parseEffect(result)
+effText = effText.replace('&#160;', '')
+effText = effText.replace('■', '>')
+effText = effText.replace('—', ' - ')
+effText = effText.replace(" \n", "\n" )
+effText = effText.replace('\n', '&#xd;&#xa;')
+effText = effText.replace('> ', '')
+effText = effText.replace(".)", ")")
+effText = effText.replace(" (", "(")
+effText = effText.replace("\"", "\'")
+print(effText)
--- extractionPrograms/Python/RevExtraction/RevExtraction.py
+++ extractionPrograms/Python/RevExtraction/RevExtraction.py
@@ -1,20 +1,41 @@
 from CardExtractor import *
 
-temp = "Dogiragon,_Flaming_Revolution"
+temp = "DMR-17_Burning_Dogiragon!!"
+
+def extractSet(setName = "kek"):
+
+	setQuery = 'https://duelmasters.fandom.com/api.php?action=query&prop=revisions&rvprop=content&format=php&titles=' + setName
+	with urllib.request.urlopen(setQuery) as response:
+		html = response.read().decode('utf-8')
+
+	i = html.find("==Contents==")
+	j = -1
+
+	endingKeywords = ["==Cycles==", "==Contents sorted by Civilizations==", "==Gallery==",  "==Trivia=="]
+	for keyword in endingKeywords:
+		j = html.find(keyword)
+		if j > 0:
+			print("Ending keyword found:"+keyword)
+			break
+	if j == -1:
+		print("WARNING: No contents ending keyword found in set data, processing everything after ==Contents==")
+
+	html = html[i:j]
+
+	lines = html.split("\n")
+	cardCount = 0
+
+	for line in lines:
+		i = line.find("[[")
+		j = line.find("]]")
+
+		if i > 0 and j > 0:
+			i = i + 2
+			cardName = line[i:j].replace(" ", "_")
+			print(str(cardCount) + " Card is: "+ cardName)
+			cardCount+= 1
+
+			print(extractCardData(cardName))
 
 
-result = extractCardData(temp)
-print(result)
-
-effText = parseEffect(result)
-effText = effText.replace('\\\\', '\\')
-effText = effText.replace('\\\\', '\\')
-effText = effText.replace('&#160;', '')
-effText = effText.replace('\\xe2\\x96\\xa0', '>')
-effText = effText.replace('\\xe2\\x80\\x94', ' - ')
-effText = effText.replace('\\n', '&#xd;&#xa;')
-effText = effText.replace('\\\'', '\'')
-effText = effText.replace('> ', '')
-effText = effText[0:-1]
-print (effText)
-
+extractSet(temp)
